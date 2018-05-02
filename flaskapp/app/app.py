9e4fc036b09b23cba9d34b33dbfacded4b596be0
PaleAle <patrick.ramseier@gmail.com>
2018-05-02 18:27:29 +0200

    Added Resource Orders with get,post,delete

--- flaskapp/app/app.py
+++ flaskapp/app/app.py
@@ -2,12 +2,29 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import reqparse, Resource, Api
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('couvert_id')
+parser.add_argument('amount')
+
+class Order_Table(db.Model):
+    __tablename__= 'order_table'
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    couvert_id = db.Column('couvert_id', db.String)
+    amount = db.Column('amount', db.Integer)
+    total_price = db.Column('total_price', db.Integer)
+
+    def __init__(self, couvert_id, amount, total_price):
+        self.couvert_id = couvert_id
+        self.amount = amount
+        self.total_price = total_price
 
 class Couvert_Table(db.Model):
     __tablename__ = 'couvert_table'
@@ -45,9 +62,65 @@ class Couvert(Resource):
         data['price'] = couvert.price
         return jsonify({'couvert': data})
 
+class OrdersList(Resource):
+    def get(self):
+        orders = Order_Table.query.all()
+        output = []
+        for order in orders:
+            data = {}
+            data['id'] = order.id
+            data['couvert_id'] = order.couvert_id
+            data['amount'] = order.amount
+            data['total_price'] = order.total_price
+            output.append(data)
+        return jsonify({'orders': output})
+
+    def post(self):
+        args = parser.parse_args()
+        if args:
+            try:
+                couvert_id = args['couvert_id']
+                amount = args['amount']
+            except Exception:
+                raise BadRequest('Please specify couvert_id and amount...')
+            #get couvert to calculate price:
+            couv= Couvert_Table.query.filter_by(id=couvert_id).first()
+            data = {}
+            data['couvert_id'] = couvert_id
+            data['amount'] = amount
+            data['total_price'] = int(amount) * int(couvert.price)
+            order = Order_Table(data['couvert_id'], data['amount'], data['total_price'])
+            db.session.add(order)
+            db.session.commit()
+            response = jsonify(data)
+            response.status_code = 201
+            return response
+        raise BadRequest('Missing POST data') 
+
+class Order(Resource):
+    def get(self, id):
+        order = Order_Table.query.filter_by(id=id).first()
+        data = {}
+        data['id'] = order.id
+        data['couvert_id'] = order.couvert_id
+        data['amount'] = order.amount
+        data['total_price'] = order.total_price
+        return jsonify({'order': data})
+
+    def delete(self, id):
+        try:
+            order = Order_Table.query.filter_by(id=id).first()
+        except Exception:
+                raise BadRequest('Order does not exist..') 
+        db.session.delete(order)
+        db.session.commit()
+        return True, 204 
+
 #routing
 api.add_resource(CouvertsList, '/couverts')
 api.add_resource(Couvert, '/couvert/<id>')
+api.add_resource(OrdersList, '/orders')
+api.add_resource(Order, '/order/<id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flaskapp/app/models.py
+++ flaskapp/app/models.py
@@ -1,6 +1,6 @@
 # create database and add some data
 from app import db
-from app import Couvert_Table
+from app import Couvert_Table, Order_Table
 
 # create database
 db.create_all()
@@ -10,8 +10,11 @@ couvert_01 = Couvert_Table(0, 'A4-Couvert', 20, 3)
 couvert_02 = Couvert_Table(1, 'A3-Couvert', 20, 5)
 couvert_03 = Couvert_Table(2, 'A2-Couvert', 20, 6)
 
+test_order = Order_Table(1, 2, 10)
+
 # commit to database
 db.session.add(couvert_01)
 db.session.add(couvert_02)
 db.session.add(couvert_03)
+db.session.add(test_order)
 db.session.commit()
